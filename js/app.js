2ab700f7d25adcf775f5d54a755effd879e3c335
Oleksìi <bardyshev@gmail.com>
2022-09-13 16:38:33 +0300

    styling was added to Submit button

--- js/app.js
+++ js/app.js
@@ -45,6 +45,23 @@ const app = new Vue({
             }
 
             return this.ticketQuantity + ' ' + readableTicketType + ' ' + ticketPluralization;
+        },
+        formIsValid: function() {
+            return this.firstName && this.lastName && this.email && this.purchaseAgreementSigned;
+        },
+        submitButtonColor: function() {
+            if (this.formIsValid) {
+                return '#4c7ef3';
+            } else {
+                return 'gray';
+            }
+        },
+        submitButtonCursor: function() {
+            if (this.formIsValid) {
+                return 'pointer';
+            } else {
+                return 'default';
+            }
         }
     },
     watch: {
